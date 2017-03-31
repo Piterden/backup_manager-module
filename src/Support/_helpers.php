2813f8facb8a10e004c598552fc91ecc6693a3a6
Piterden <efremov.a.denis@gmail.com>
2017-03-31 12:05:12 +0300

    Fixed a typo

--- src/Support/_helpers.php
+++ src/Support/_helpers.php
@@ -15,6 +15,6 @@ if (!function_exists('human_filesize'))
     {
         $size   = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
         $factor = floor((strlen($bytes) - 1) / 3);
-        return sprintf("%.{ $decimals}f", $bytes / pow(1024, $factor)).@$size[$factor];
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)).@$size[$factor];
     }
 }
